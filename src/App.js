7235719bfa444bca1efa84fe4c274975309e440a
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-05-14 16:38:55 +0330

    -

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ function App() {
    let[number,setNumber] = useState('0');
 
    const handleDigit =(d) =>{
-       
-   } 
+       if(number == '0')
+          setNumber(d)
+       else
+       setNumber(`${number}${d}`)
+   }
+   
+   useEffect(() =>{
+      const input = document.querySelector(".calculator__input")
+   })
  return(
 
  );
